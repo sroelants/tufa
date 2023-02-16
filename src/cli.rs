eb03bd3960510ae024526640fa9d24106e365522
Sam Roelants <sam@samroelants.com>
2023-02-16 11:27:50 +0100

    Add clap cli

---
+++ src/cli.rs
@@ -0,0 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Add a 2FA service
+    Add {
+        /// The name of the service you'd like to add 2FA for
+        service: String,
+
+        /// The 2FA secret provided by the service
+        secret: String,
+    },
+
+    // Remove a service
+    Remove {
+        /// The service you'd like to remove
+        service: String,
+    },
+
+    /// Generate a new 2FA code
+    Gen {
+        /// The service you'd like to generate a code for
+        service: String,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 mod otp;
 mod db;
-mod models;
-mod schema;
+mod crypto;
+mod cli;
+mod util;
+
+use clap::Parser;
+use cli::{Cli, Commands};
+use db::models::service;
+use diesel::SqliteConnection;
 
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let mut connection = db::establish_connection();
+
+    match &cli.command {
+        Some(Commands::Add { service, secret }) => { add_service(&mut connection, service, secret)}
+        Some(Commands::Gen { service }) => { get_service(&mut connection, service)}
+        _ => {}
+        // Some(Commands::Remove { service }) => {}
+        // None => {}
+    }
+}
+
+fn add_service(conn: &mut SqliteConnection, name: &str, secret: &str) {
+    let service = service::create(conn, name, secret);
+    println!("{:?}", service);
+}
+
+fn get_service(conn: &mut SqliteConnection, name: &str) {
+    let service = service::get_by_name(conn, name);
+    println!("{:?}", service);
 }
