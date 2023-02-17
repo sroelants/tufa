c33c8bd0e72775b37b8a025111d0f1bd9ecdd678
Sam Roelants <sam@samroelants.com>
2023-02-17 10:15:30 +0100

    Include migrations into binary

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tufa
+
+A simple 2-factor authentication generator.
+**WARNING**: For the time being, 2FA secrets are stored as plaintext, which is 
+a serious security consideration to take note of..
+
+Left to do:
+- [ ] Encrypt secrets with master password
+- [ ] Actual error handling
+- [ ] Confirmation prompting
+- [ ] `tufa ls`
+- [ ] rename to `tufa rm`
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,17 +1,43 @@
 use diesel::sqlite::SqliteConnection;
 use diesel::prelude::*;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use diesel::sqlite::Sqlite;
 use dotenv::dotenv;
-use std::env;
+use std::env::{self, VarError};
+use std::error::Error;
+
 pub mod models;
 pub mod schema;
 
 
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+
+fn run_migrations(connection: &mut impl MigrationHarness<Sqlite>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    connection.run_pending_migrations(MIGRATIONS)?;
+    Ok(())
+}
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")
+        .or(get_xdg_config_path())
         .expect("DATABASE_URL must be set.");
 
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    let mut connection = SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+
+    run_migrations(&mut connection).unwrap();
+
+    connection
+}
+
+fn get_xdg_config_path() -> Result<String, VarError> {
+    env::var("XDG_CONFIG_HOME")
+        .map(|dir| std::path::Path::new(&dir)
+            .join("./tufa.db")
+            .to_str()
+            .unwrap()
+            .to_owned()
+        )
 }
